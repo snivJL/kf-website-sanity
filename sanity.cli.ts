1b71888ce2deaef712137cfa58d53d6178ba16df
Julien <julien.lejay@gmail.com>
2025-06-04 16:54:06 +0700

    chore: default studio host

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,11 +3,12 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '7gk1pdfh',
-    dataset: 'production'
+    dataset: 'production',
   },
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
   autoUpdates: true,
+  studioHost: 'kf-website',
 })
